18e0726748600ca048bb487dbea08328cf825497
H2CK <t@jagel.net>
2022-03-09 13:30:39 +0100

    Improved discovery controller test

--- tests/Unit/Controller/DiscoveryControllerTest.php
+++ tests/Unit/Controller/DiscoveryControllerTest.php
@@ -51,18 +51,98 @@ class DiscoveryControllerTest extends TestCase {
 	}
 
 	public function testDiscoveryResponse() {
-		$scopesSupported = [
+		$issuer = $this->request->getServerProtocol() . '://' . $this->request->getServerHost() . $this->urlGenerator->getWebroot();
+        $scopesSupported = [
             'openid',
             'profile',
             'email',
             'roles',
             'groups',
         ];
+        $responseTypesSupported = [
+            'code',
+            // 'code id_token',
+            // 'code token',
+            // 'code id_token token',
+            // 'id_token',
+            // 'id_token token'
+        ];
+        $responseModesSupported = [
+            'query',
+            // 'fragment',
+        ];
+        $grantTypesSupported = [
+            'authorization_code',
+            // 'implicit',
+        ];
+        $acrValuesSupported = [
+            '0',
+        ];
+        $subjectTypesSupported = [
+            // 'pairwise',
+            'public',
+        ];
+        $idTokenSigningAlgValuesSupported = [
+            'RS256',
+            'HS256',
+        ];
+        $userinfoSigningAlgValuesSupported = [
+            'none',
+        ];
+        $tokenEndpointAuthMethodsSupported = [
+            'client_secret_post',
+            // 'client_secret_basic',
+            // 'client_secret_jwt',
+            // 'private_key_jwt',
+        ];
+        $displayValuesSupported = [
+            'page',
+            // 'popup',
+            // 'touch',
+            // 'wap',
+        ];
+        $claimTypesSupported = [
+            'normal',
+            // 'aggregated',
+            // 'distributed',
+        ];
+        $claimsSupported = [
+            'iss',
+			'sub',
+			'aud',
+			'exp',
+			'auth_time',
+			'iat',
+			'acr',
+			'azp',
+			'preferred_username',
+			'scope',
+			'nbf',
+			'jti',
+            'roles',
+            'name',
+            'updated_at',
+            'website',
+            'email',
+            'email_verified',
+        ];
 
 		$result = $this->controller->getInfo();
 
 		$this->assertEquals(Http::STATUS_OK, $result->getStatus());
+		$this->assertEquals($issuer, $result->getData()['issuer']);
 		$this->assertEquals($scopesSupported, $result->getData()['scopes_supported']);
+		$this->assertEquals($responseTypesSupported, $result->getData()['response_types_supported']);
+		$this->assertEquals($responseModesSupported, $result->getData()['response_modes_supported']);
+		$this->assertEquals($grantTypesSupported, $result->getData()['grant_types_supported']);
+		$this->assertEquals($acrValuesSupported, $result->getData()['acr_values_supported']);
+		$this->assertEquals($subjectTypesSupported, $result->getData()['subject_types_supported']);
+		$this->assertEquals($idTokenSigningAlgValuesSupported, $result->getData()['id_token_signing_alg_values_supported']);
+		$this->assertEquals($userinfoSigningAlgValuesSupported, $result->getData()['userinfo_signing_alg_values_supported']);
+		$this->assertEquals($tokenEndpointAuthMethodsSupported, $result->getData()['token_endpoint_auth_methods_supported']);
+		$this->assertEquals($displayValuesSupported, $result->getData()['display_values_supported']);
+		$this->assertEquals($claimTypesSupported, $result->getData()['claim_types_supported']);
+		$this->assertEquals($claimsSupported, $result->getData()['claims_supported']);
 	}
 
 
