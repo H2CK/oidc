77c86e970c7f3ca869a26f1cf98e8651cf8a9af5
Julien Veyssier <julien-nc@posteo.net>
2025-02-13 14:29:43 +0100

    also validate ID tokens in TokenValidationRequestListener
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Event/TokenValidationRequestEvent.php
+++ lib/Event/TokenValidationRequestEvent.php
@@ -11,7 +11,7 @@ namespace OCA\OIDCIdentityProvider\Event;
 use OCP\EventDispatcher\Event;
 
 /**
- * This event is emitted by other apps that want to know if an access token is valid
+ * This event is emitted by other apps that want to know if an access/id token is valid
  */
 class TokenValidationRequestEvent extends Event {
 
@@ -19,13 +19,13 @@ class TokenValidationRequestEvent extends Event {
     private ?string $userId = null;
 
     public function __construct(
-        private string $accessToken,
+        private string $token,
     ) {
         parent::__construct();
     }
 
-    public function getAccessToken(): string {
-        return $this->accessToken;
+    public function getToken(): string {
+        return $this->token;
     }
 
     public function getIsValid(): ?bool {
