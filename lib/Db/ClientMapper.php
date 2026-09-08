c6fcf64388d7ba67aed75fe16f1cee57eeff9546
H2CK <t@jagel.net>
2026-09-08 08:50:12 +0200

    Fix MariaDB incompatibility on boolean
    Signed-off-by: H2CK <t@jagel.net>

--- lib/Db/ClientMapper.php
+++ lib/Db/ClientMapper.php
@@ -273,4 +273,22 @@ class ClientMapper extends QBMapper {
             ->where($where);
         $qb->executeStatement();
     }
+
+    /**
+    * Bind client feature flags as integers for MariaDB compatibility.
+     * Some Nextcloud/DBAL combinations serialize false as an empty string for
+     * nullable boolean columns, which strict MariaDB rejects for TINYINT(1).
+     */
+    protected function getParameterTypeForProperty(Entity $entity, string $property) {
+        if ($entity instanceof Client && in_array($property, [
+            'dcr',
+            'texEnabled',
+            'backchannelLogoutSessReq',
+            'frontchannelLogoutSessReq',
+        ], true)) {
+            return IQueryBuilder::PARAM_INT;
+        }
+
+        return parent::getParameterTypeForProperty($entity, $property);
+    }
 }
